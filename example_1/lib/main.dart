216d9cce2c73108c53e9768ae251928975ee8c69
samieruashovo <samieruashovo@gmail.com>
2024-03-22 23:38:37 +0600

    example_1

---
+++ example_1/lib/main.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'dart:math' show pi;
+
+void main() {
+  runApp(
+    const App(),
+  );
+}
+
+class App extends StatefulWidget {
+  const App({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    _controller = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    );
+    _animation = Tween(begin: 0.0, end: 2 * pi).animate(_controller)
+      ..addListener(() {
+        setState(() {});
+      })
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          _controller.repeat();
+        }
+      });
+    _controller.repeat();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(brightness: Brightness.dark),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      themeMode: ThemeMode.dark,
+      debugShowCheckedModeBanner: false,
+      debugShowMaterialGrid: false,
+      home: Scaffold(
+        body: Center(
+          child: AnimatedBuilder(
+            animation: _controller,
+            builder: (context, child) {
+              return Transform(
+                alignment: Alignment.center,
+                transform: Matrix4.identity()..rotateZ(_animation.value),
+                child: Container(
+                  width: 100,
+                  height: 100,
+                  decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: const BorderRadius.all(Radius.circular(10)),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.5),
+                          blurRadius: 10,
+                          spreadRadius: 5,
+                          offset: const Offset(0, 3),
+                        )
+                      ]),
+                ),
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
